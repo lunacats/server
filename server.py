c1047fc0f86919cfe267ce3b26d974333a9ca0a7
harshrealm <luncats@github.com>
2018-03-11 18:53:15 -0400

    added arduino interface

--- server.py
+++ server.py
@@ -1,30 +1,53 @@
 #!/usr/bin/env python
 import socket
+import serial
+# replace [serial_item] with output of ls dev/tty*
+# https://oscarliang.com/connect-raspberry-pi-and-arduino-usb-cable/
+# second argument is baud rate
+ser = serial.Serial('/dev/tty4', 9600)
+# to avoid interpreting noise, these are the first two bytes of every command
+COMMAND_BYTE_PAIR = bytearray(b'\xFF\xEE')
+
+def sendToArduio(command):
+	print(COMMAND_BYTE_PAIR.append(command))
+	ser.write(COMMAND_BYTE_PAIR.append(command))
 # functions to interface with arduino
 def button1():
+	sendToArduio(bytearray(b'\xB1'))
 	print("Button 1 command recieved")
 def button2():
 	print("Button 2 command recieved")
+	sendToArduio(bytearray(b'\xB2'))
 def button3():
 	print("Button 3 command recieved")
+	sendToArduio(bytearray(b'\xB3'))
 def button4():
 	print("Button 4 command recieved")
+	sendToArduio(bytearray(b'\xB4'))
 def button5():
 	print("Button 5 command recieved")
+	sendToArduio(bytearray(b'\xB5'))
 def button6():
 	print("Button 6 command recieved")
+	sendToArduio(bytearray(b'\xB6'))
 def button7():
+	sendToArduio(bytearray(b'\xB7'))
 	print("Button 7 command recieved")
 def button8():
 	print("Button 8 command recieved")
+	sendToArduio(bytearray(b'\xB8'))
 def button9():
 	print("Button 9 command recieved")
+	sendToArduio(bytearray(b'\xB9'))
 def button10():
 	print("Button 10 command recieved")
+	sendToArduio(bytearray(b'\xB0'))
 def button11():
 	print("Button 11 command recieved")
+	sendToArduio(bytearray(b'\xBA'))
 def button12():
 	print("Button 12 command recieved")
+	sendToArduio(bytearray(b'\xBB'))
 	
 # byte assignments for commands TODO
 commands = {
